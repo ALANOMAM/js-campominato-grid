22ef98d12e0eba0440f4b9e2dc07a464f80cd7fc
alan_omam <omamalan@hotmail.it>
2024-02-08 11:49:28 +0100

    FINE PRIMA PARTE(sistemato il fatto che le tabelle si aggiungevano una sotto l'altra al cambio di livello. Reso la grid quadrata)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata 
 si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità
+
 */
 
 
@@ -17,19 +18,38 @@ pulsante.addEventListener("click", function(){
     let griglia = document.querySelector("#grid")
     let livelli = document.querySelector("#levels")
    
+    //mi resetta la tabella ogni volta che premo "gioca" cosi le tabelle non si aggiungano una sotto l'altra quando cambio livelli di difficolà
+     griglia.innerHTML=""
+
+ // mi cambia il numero di scatole a disposizione a seconda del livello     
 if(livelli.value == "level1"){
      num = 100
+     
 }else if(livelli.value == "level2"){
     num = 81
 }else{
   num=49
 }
-    
+   
+
 for(let i=0; i<num;i++){
     //creo i vari div   
    let scatole = document.createElement("div")
-   //aggiungo qualche stile ad ogni scatola nella grid
+  
+   //STILE PER LE SCATOLE DENTRO IL MIO GRID
+   //collego la classe box ad ogni scatola per avere una prima stilizazione
    scatole.classList.add("box")
+   
+   //aggiungo anche le classi "easy" "medium" o "hard" alle mie scatole a seconda del livello 
+   if(livelli.value == "level1"){
+     scatole.className += " easy"  
+  }else if(livelli.value == "level2"){
+    scatole.className += " medium"
+  }else{
+    scatole.className += " hard"
+  }
+
+   
    //aggiungo i numeri dentro le scatole
    scatole.innerHTML = (i+1)
    
@@ -47,7 +67,5 @@ for(let i=0; i<num;i++){
    griglia.append(scatole)
    
    }
-   
-   /*console.log(griglia)*/
 
 })
